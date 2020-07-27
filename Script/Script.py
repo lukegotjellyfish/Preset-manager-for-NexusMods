e5dfbd19e0402a885932221bbb81489d29fc2609
lukegotjellyfish <lukegotjellyfish@gmail.com>
2020-07-27 18:10:55 +0100

    Update for catDict and using category conversion

--- Script/Script.py
+++ Script/Script.py
@@ -2,75 +2,13 @@
 import re
 import os
 import html
+import catDict
 import tkinter as tk
 from tkinter import filedialog
 
-
-categoryDictionary = {
-	"22": "Buildings",
-	"24": "Gameplay Effects and Changes",
-	"25": "Guilds/Factions",
-	"26": "Body, Face, and Hair",
-	"27": "Items and Objects - Player",
-	"28": "Miscellaneous",
-	"29": "Models and Textures",
-	"30": "New Lands",
-	"33": "NPC",
-	"34": "Races, Classes, and Birthsigns",
-	"35": "Quests and Adventures",
-	"36": "Weapons and Armour",
-	"39": "Utilities",
-	"40": "Cheats and God items",
-	"42": "User Interface",
-	"43": "Save Games",
-	"45": "Videos and Trailers",
-	"51": "Animation",
-	"53": "Cities, Towns, Villages, and Hamlets",
-	"54": "Armour",
-	"55": "Weapons",
-	"58": "Landscape Changes",
-	"60": "Clothing",
-	"61": "Audio - Music",
-	"62": "Visuals and Graphics",
-	"65": "Followers & Companions - Creatures",
-	"67": "Player homes",
-	"68": "Castles, Palaces, Mansions, and Estates",
-	"69": "Mercantiles (shops, stores, inns, taverns, etc)",
-	"70": "Forts, Ruins, and Abandoned Structures",
-	"73": "Skills and Leveling",
-	"74": "Environmental",
-	"75": "Magic - Spells & Enchantments",
-	"76": "Stealth",
-	"77": "Combat",
-	"78": "Immersion",
-	"79": "Overhauls",
-	"82": "Modders Resources and Tutorials",
-	"83": "Creatures",
-	"84": "Patches",
-	"85": "Items and Objects - World",
-	"88": "Dungeons - New",
-	"89": "Locations - New",
-	"90": "Locations - Vanilla",
-	"91": "Dungeons - Vanilla",
-	"92": "Collectables, Treasure Hunts, and Puzzles",
-	"93": "Magic - Gameplay",
-	"94": "Alchemy",
-	"95": "Bug Fixes",
-	"96": "Followers & Companions",
-	"97": "Presets - ENB",
-	"98": "Books and Scrolls",
-	"99": "NPC - Children",
-	"100": "Crafting",
-	"101": "Mounts",
-	"102": "Clothing - Jewelry",
-	"103": "Armour - Shields",
-	"104": "Shouts",
-	"105": "Presets - ReShade",
-	"106": "Audio - SFX",
-	"107": "Audio - Voice",
-	"108": "VR",
-	"109": "Character Presets"
-}
+def categoryTranslate(category, game):
+	if (game == "skyrimse"):
+		return catDict.skyrimSE[str(category)]
 
 def trimList(x,type):
 	x[0] = x[0].replace('        "name": "',"")[:-3]
@@ -87,8 +25,10 @@ def trimList(x,type):
 	if type == 2:  #Has endorsed section
 		x[8] = x[8].replace('        "game": "',"")[:-3]
 		x[9] = x[9].replace('        "endorsed": "',"")[:-2]
-	return x
+	print(x[8])
+	x[2] = categoryTranslate(x[2], x[8])
 
+	return x
 
 
 while True:
