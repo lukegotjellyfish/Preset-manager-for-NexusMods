e78698212bb2959859b70cc65390a072fe78cdd9
lukegotjellyfish <lukegotjellyfish@gmail.com>
2020-07-28 09:23:54 +0100

    Update to create nested dictionaries
    Infinitely more practical to reference from main script

--- Script/GetCategoriesFromNexus/NexusGetCategories.py
+++ Script/GetCategoriesFromNexus/NexusGetCategories.py
@@ -1,17 +1,24 @@
-from bs4 import BeautifulSoup
-from urllib.request import Request, urlopen
-from lxml import html
 import requests
 import time
 import re
+import ast
+import random
+import os
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+from lxml import html
 
-dictList  = []
 
-def add_another(input_url, filename):
+#Function repurposed from SteamCollectionSizeFinder
+def add_another(input_url, gamename):
+    #Request URL with Firefox identity
     req = Request(input_url, headers={'User-Agent': 'Mozilla/5.0'})
+    #Open page to var
     html_page = urlopen(req)
+    #Init BS4 with page as lxml
     soup = BeautifulSoup(html_page, "lxml")
 
+    #Text replacement list
     replacement = ["<ul class=\"choice-list\">\n",
     "\n<input id=\"has_images\" name=\"has_images\" type=\"checkbox\" value=\"1\"/>",
     "\n<label for=\"has_images\">",
@@ -28,28 +35,56 @@ def add_another(input_url, filename):
     "\n<span class=\"check-label\">Only show adult files</span>",
     "<li>\n",
     "\n</ul>"]
+    #Get all choice-lists (categories and attributes)
     alls = soup.find_all("ul", {"class": "choice-list"})
+    #First list is categories
     temp = str(alls[0])
+    #Iterate through replacement strings to remove them from temp
     for x in replacement:
         temp = temp.replace(x,"")
+    #Use regex rules to further strip category list into a usable format
     temp = re.sub("<span class=\"check-category\">.*</span>\n", "", temp)
+    temp = re.sub("<span class=\"check-category\">.*</span>", "", temp)
     temp = re.sub("<input id=\"cat.*\" name=\"categories\[\]\" type=\"checkbox\" value=\".*\"/>\n", "", temp)
-    temp = re.sub("<label for=\"cat(.*)\">\n<span class=\"check-label\">(.*)</span>", "	\"\g<1>\": \"\g<2>\",", temp)
-    #temp = re.sub("\n", "", temp)
-    temp = filename + " = {\n" + temp[:-1] + "\n}\n"
-    print(temp)
-    dictList.append(temp)
+    temp = re.sub("<label for=\"cat(.*)\">\n<span class=\"check-label\">(.*)</span>", "\"\g<1>\": \"\g<2>\",", temp)
 
+    #Create evalTemp as a string dictionary
+    evalTemp = "{" + temp[:-1] + "}"
+    #Eval evalTempt as a dictionary using ast
+    tempDict = ast.literal_eval(evalTemp)
+    #Sort dictionary by key values
+    tempDict = sorted(tempDict.items())
+    #Convert dictionary into string to format and save
+    stringDict = str(tempDict)
 
+    #Remove dictionary items from (x:y),(z:c) format
+    stringDict = re.sub("\('([^\)]*)', '([^\)]*)'\)", "\n		\"\g<1>\": \"\g<2>\"", stringDict)
+    #Strip leading and ending [], replace with python dictionary format
+    stringDict = stringDict.replace("[", "	" + gamename + " = {").replace("]","\n	}\n")
+    #Print dictionary to preview
+    print(stringDict)
+   
+    with open(filename, "a") as f:
+        f.write(stringDict)
 
 ##START##
+filename = "dictionaries.txt"
+if os.path.isfile("./dictionaries.txt"):
+    x = 1
+    while os.path.isfile("./dictionaries" + str(x) + ".txt") is True:
+        x += 1
+    filename = "dictionaries" + str(x) + ".txt"
+with open(filename, "w") as f:
+    f.write("nexus_dictionaries = {\n")
+
 with open("links.txt", "r") as f:
     lines = f.readlines()
     for line in lines:
         title = line.replace("https://www.nexusmods.com/","").replace("/mods","").replace("\n","")
         add_another(line, title)
-        time.sleep(2)
+        #1 second delay to be kind to nexus's servers
+        time.sleep(1)
 
-with open("dictionaries.txt", "w") as f:
-    for item in dictList:
-        f.write(item)
+#end dict
+with open(filename, "a") as f:
+    f.write("\n}")
